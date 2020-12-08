cbfa100b569463cc74795698c9fcd171e002ffb9
Andrzej Glomski <andrzejglomski2@gmail.com>
2020-12-08 09:51:53 +0100

    Add buttons to game

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
-function getMoveName(argMoveId){
-    if(argMoveId == 1){
-      return 'kamień';
-    }
-    else if (argMoveId==2){
-        return "papier";
-    }
-    else if (argMoveId==3){
-        return "nozyce";
-    }
-    else{
-    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-    printMessage("Wybierz pomiedzy  1: kamień, 2: papier, 3: nożyce.")};
+function playGame(playerInput){ 
+    clearMessages();
+    function getMoveName(playerMove){
+        if(playerMove == 1){
+        return 'kamień';
+        }
+        else if (playerMove==2){
+            return "papier";
+        }
+        else if (playerMove==3){
+            return "nozyce";
+        }
+        else{
+        printMessage('Nie znam ruchu o id ' + + '.');
+        printMessage("Wybierz pomiedzy  1: kamień, 2: papier, 3: nożyce.")};
 }
 
 
@@ -20,23 +22,13 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = getMoveName(randomNumber);
-/*
-if(randomNumber == 1){
-  computerMove = 'kamień';
-}
-else if(randomNumber==2){
-    computerMove="papier";
-}
-else if(randomNumber==3){
-    computerMove="nozyce";
-}
-*/
-printMessage('Mój ruch to: ' + computerMove);
 
+printMessage('Mój ruch to: ' + computerMove);
+/*
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
-
+*/
 let playerMove = getMoveName(playerInput);
 
 
@@ -73,3 +65,19 @@ function displayResult(playerMove,computerMove){
     }
 }
 printMessage(displayResult(playerMove,computerMove));
+}
+
+let playPaper=document.getElementById("play-paper");
+playPaper.addEventListener("click",function(){
+ playGame();
+})
+
+let playScisors=document.getElementById("play-scisors");
+playScisors.addEventListener("click",function(){
+  printMessage("");
+});
+
+let playRock=document.getElementById("play-rock");
+playRock.addEventListener("click",function(){
+  printMessage("");
+});
